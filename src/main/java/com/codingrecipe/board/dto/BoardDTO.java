9d3788850aa04ba57c9487f52bd8e1169c55e300
Pai <clsrntkdgh@naver.com>
2024-02-21 13:29:00 +0900

    first commit

---
+++ src/main/java/com/codingrecipe/board/dto/BoardDTO.java
@@ -0,0 +1,40 @@
+package com.codingrecipe.board.dto;
+
+import com.codingrecipe.board.entity.BoardEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+// DTO(Data Transfer Object) , VO, Bean,        Entity
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor    //기본 생성자
+@AllArgsConstructor   // 모든 필드를 매개변수로 하는 생성자
+public class BoardDTO {
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContents;
+    private int boardHits;
+    private LocalDateTime boardCreatedTime; // 게시글 작성 시간
+    private LocalDateTime boardUpdatedTime; // 게시글 수정 시간
+
+
+    public static BoardDTO toBoardDTO(BoardEntity boardEntity){
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setBoardCreatedTime(boardEntity.getCreatedTime());
+        boardDTO.setBoardUpdatedTime(boardEntity.getUpdatedTime());
+        return boardDTO;
+    }
+
+
+}
