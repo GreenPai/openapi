5b0be9f2dafe099be0dba837ddfa807653c8ee82
Pai <clsrntkdgh@gmail.com>
2024-03-29 21:17:45 +0900

    이미지 추출 오류. 이미지 안나옴..

--- src/main/java/com/codingrecipe/board/config/WebConfig.java
+++ src/main/java/com/codingrecipe/board/config/WebConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+
     private String resourcePath = "/upload/**"; // view 에서 접근할 경로
     private String savePath = "file:///C:/springboot_img/"; // 실제 파일 저장 경로(win)
 //    private String savePath = "file:///Users/사용자이름/springboot_img/"; // 실제 파일 저장 경로(mac)
@@ -15,4 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler(resourcePath)
                 .addResourceLocations(savePath);
     }
+
+
+
 }
